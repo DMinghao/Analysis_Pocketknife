fe15e5d56d9882211efea3694d2c5c9d20cc40fe
DMinghao <45401748+DMinghao@users.noreply.github.com>
2021-12-13 12:14:29 -0500

    Update clear_env.R

--- R/clear_env.R
+++ R/clear_env.R
@@ -4,8 +4,6 @@ rm(list = ls())
 
 gc()
 
-.rs.restartR()
-
 if (!require(downloader))
   install.packages("downloader")
 
@@ -15,3 +13,5 @@ downloader::source_url(
     "https://raw.githubusercontent.com/DMinghao/Analysis_Pocketknife/main/R/import.R"
   )
 )
+
+.rs.restartR()
